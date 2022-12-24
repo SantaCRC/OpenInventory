e131ea25606e3d9cb52df7f4845643f95d0700c2
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2022-12-23 18:23:35 -0600

    fix project

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from dashboard.models import Product, Category, Settings, Storage
+from dashboard.models import Product, Category, Settings, Storage, Project
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(Settings)
 admin.site.register(Storage)
+admin.site.register(Project)
